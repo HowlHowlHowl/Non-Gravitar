7cd6b8ffaf13a69602433ae110c34a0bf7c9f702
ramenguy99 <ramenguy99@gmail.com>
2019-03-08 17:31:20 +0100

    Aggiunto test con finestra

--- Gravitar/Gravitar/main.cpp
+++ Gravitar/Gravitar/main.cpp
@@ -11,9 +11,23 @@ const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* args[])
 {
-	int windowWidth = 1024;
-	int windowHeight = 768;
-	RenderWindow window(VideoMode(windowWidth, windowHeight), "Gravitar");
-	
+	RenderWindow window(VideoMode(200, 200), "SFML works!");
+	CircleShape shape(100.f);
+	shape.setFillColor(Color::Green);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
